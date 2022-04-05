0bb20b276c3ff711bf1ed84aa5730c552e881aa8
Jose Alberto Lorenzo Sánchez <josealberto.lorenzo@amaris.com>
2022-04-05 17:26:47 +0200

    add functionality of mark as paid the carts

--- src/main/java/com/example/adapters/in/gui/CartControllerWeb.java
+++ src/main/java/com/example/adapters/in/gui/CartControllerWeb.java
@@ -5,8 +5,11 @@ import com.example.ports.in.CartCRUDInPort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class CartControllerWeb {
@@ -22,5 +25,17 @@ public class CartControllerWeb {
         List<Cart> carts = this.cartPort.getAllCarts();
         model.addAttribute("carts", carts);
         return "cart-list";
+    }
+
+    @RequestMapping(value = "/carts/{id}/paid")
+    public String markAsPaid(@PathVariable(value = "id") Long id){
+
+        if (id > 0 ){
+            final UUID idPayment = UUID.randomUUID();
+            var cart = this.cartPort.findById(id);
+            cart.setIdPayment(idPayment);
+            this.cartPort.update(cart);
+        }
+        return "redirect:/carts";
     }
 }
